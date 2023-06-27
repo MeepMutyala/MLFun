712a400ad3f27ca0f5683abcb8b68bde1c622496
Apuroop Mutyala <apumutyala@gmail.com>
2023-06-27 16:44:08 -0400

    data ingestion complete

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 from setuptools import find_packages, setup
 
+E_Var = '-e .'
+
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return package requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replaces("\n", "") for req in requirements]
+
+        if E_Var in requirements:
+            requirements.remove(E_Var)
+            
+    return requirements
+
 
 
 setup(
